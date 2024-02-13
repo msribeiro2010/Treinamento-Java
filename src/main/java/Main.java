8c8ed5592cf8e4eec149523bc7d8a224bca28771
msribeiro2010 <msribeiro2010@icloud.com>
2024-02-13 12:33:33 +0000

    comentario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,25 @@
-  // tabuada de um numero usando scanner e recursividade
-  // import org.junit.jupiter.api.Test;
-  
-  
-  import java.util.Scanner;
-  
-  public class Main {
-  
-      public static void main(String[] args) {
-  
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite um número para ver sua tabuada: ");
-        int multiplicando = scanner.nextInt();
-  
-        imprimirTabuada(multiplicando, 0);
-      }
-  
-       static void imprimirTabuada(int multiplicando, int multiplicador) {
-  
-         if(multiplicador <= 10) {
-           System.out.println(multiplicando + " x " + multiplicador + " = " + (multiplicando * multiplicador));
-           imprimirTabuada(multiplicando, multiplicador + 1);
-         }
-  
-  
-       }
+// tabuada de um numero usando scanner e recursividade
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite um número para ver sua tabuada: ");
+    int multiplicando = scanner.nextInt();
+    // imprimindo a tabuada
+    imprimirTabuada(multiplicando, 0);
   }
+
+  static void imprimirTabuada(int multiplicando, int multiplicador) {
+
+    if (multiplicador <= 10) {
+      System.out.println(multiplicando + " x " + multiplicador + " = " + (multiplicando * multiplicador));
+      imprimirTabuada(multiplicando, multiplicador + 1);
+    }
+
+  }
+}
